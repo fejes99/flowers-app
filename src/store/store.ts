5eb5a9a3da51059aa502bf7d051f1699ec775f48
David Fejes <35314157+fejes99@users.noreply.github.com>
2023-03-21 10:59:39 +0100

    Updated StoreAction type

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,17 +1,27 @@
-import { applyMiddleware, combineReducers, compose, createStore, Reducer } from 'redux';
+import {
+  applyMiddleware,
+  combineReducers,
+  compose,
+  legacy_createStore as createStore,
+  Reducer,
+} from 'redux';
 import thunk, { ThunkMiddleware } from 'redux-thunk';
 
 import FlowerState from '../Flowers/State/flowerState';
 import flowersReducer from '../Flowers/State/flowersReducer';
 import AuthState from '../auth/State/authState';
 import authReducer from '../auth/State/authReducer';
+import { AuthActionTypes } from '../auth/State/authTypes';
+import { FlowerActionTypes } from '../Flowers/State/flowerTypes';
 
 export interface StoreState {
   flowers: FlowerState;
   auth: AuthState;
 }
 
-const rootReducer: Reducer<StoreState, any> = combineReducers<StoreState>({
+type StoreAction = FlowerActionTypes | AuthActionTypes;
+
+const rootReducer: Reducer<StoreState, StoreAction> = combineReducers<StoreState>({
   flowers: flowersReducer,
   auth: authReducer,
 });
